56b4449aa6d220eb286ec7be33deea321a041416
NandhaKishore2001 <nk387543@gmail.com>
2022-02-27 08:44:57 +0530

    first commit

---
+++ client/src/pizzasdata.js
@@ -0,0 +1,112 @@
+
+const pizzas=
+[{
+    name:"PEPPER BARBECUE CHICKEN",
+    variants:[
+        "small",
+        "medium",
+        "large"
+    ],
+    prices:[
+        {
+            "small":200,
+            "medium":350,
+            "large":400
+        }
+    ],
+    category:"nonveg",
+    image:"https://www.vegrecipesofindia.com/wp-content/uploads/2020/11/pizza-recipe.jpg",
+    description:"Pepper barabecue chicken i cheese",
+},
+{
+    name:"PEPPER BARBECUE CHICKEN",
+    variants:[
+        "small",
+        "medium",
+        "large"
+    ],
+    prices:[
+        {
+            "small":200,
+            "medium":350,
+            "large":400
+        }
+    ],
+    category:"nonveg",
+    image:"https://www.onmanorama.com/content/dam/mm/en/food/features/images/2021/10/17/pizza.jpg.transform/onm-articleimage/image.jpg",
+    description:"Pepper barabecue chicken i cheese",
+},
+{
+    name:"PEPPER BARBECUE CHICKEN",
+    variants:[
+        "small",
+        "medium",
+        "large"
+    ],
+    prices:[
+        {
+            "small":200,
+            "medium":350,
+            "large":400
+        }
+    ],
+    category:"nonveg",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/375px-Eq_it-na_pizza-margherita_sep2005_sml.jpg",
+    description:"Pepper barabecue chicken i cheese",
+},
+{
+    name:"PEPPER BARBECUE CHICKEN",
+    variants:[
+        "small",
+        "medium",
+        "large"
+    ],
+    prices:[
+        {
+            "small":200,
+            "medium":350,
+            "large":400
+        }
+    ],
+    category:"nonveg",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/375px-Eq_it-na_pizza-margherita_sep2005_sml.jpg",
+    description:"Pepper barabecue chicken i cheese",
+},
+{
+    name:"PEPPER BARBECUE CHICKEN",
+    variants:[
+        "small",
+        "medium",
+        "large"
+    ],
+    prices:[
+        {
+            "small":200,
+            "medium":350,
+            "large":400
+        }
+    ],
+    category:"nonveg",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/375px-Eq_it-na_pizza-margherita_sep2005_sml.jpg",
+    description:"Pepper barabecue chicken i cheese",
+},
+{
+    name:"PEPPER BARBECUE CHICKEN",
+    variants:[
+        "small",
+        "medium",
+        "large"
+    ],
+    prices:[
+        {
+            "small":200,
+            "medium":350,
+            "large":400
+        }
+    ],
+    category:"nonveg",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/375px-Eq_it-na_pizza-margherita_sep2005_sml.jpg",
+    description:"Pepper barabecue chicken i cheese",
+},
+]
+export default pizzas
